82f58eca4c7563b5ff709e26766a4569f1413654
Pranay Mittal <pranaym12@gmail.com>
2019-01-05 02:15:00 -0800

    Add feature to add in exceptions (one week or less at a time). Also fix bug where I check for the wrong part of a dict. line 152.

--- servery.js
+++ servery.js
@@ -1,5 +1,6 @@
 class Servery {
     constructor(name, dayDict) {
+        //Names are: Baker, SidRich, South, West, North, Seibel
         this.name = name;
         this.dayDict = {
             "Friday": dayDict["Friday"],
@@ -11,7 +12,9 @@ class Servery {
         let avgB = [7.5, 10.5];
         let avgL = [11.5, 13.5];
         let avgD = [17.5, 19.5];
-        //intiialize times for Mond - Thurs
+
+        //this.times is an array with the times ordered [Sun,M,Tu,W,Th,F,Sa]
+        //intiialize times for Sunday
         this.times = [];
         if(this.dayDict["Sunday"]){
             //this.times[0] = {"Lunch": [11.5, 14], "Dinner": [17, 19]};
@@ -20,30 +23,68 @@ class Servery {
         else{
             this.times.push(null);
         }
-        // this.times = {
-        //     1: {"Breakfast": avgB.slice(), "Lunch": avgL.slice(), "Dinner": avgD.slice()}, 
-        //     2: {"Breakfast": avgB.slice(), "Lunch": avgL.slice(), "Dinner": avgD.slice()}, 
-        //     3: {"Breakfast": avgB.slice(), "Lunch": avgL.slice(), "Dinner": avgD.slice()}, 
-        //     4: {"Breakfast": avgB.slice(), "Lunch": avgL.slice(), "Dinner": avgD.slice()}
-        // };
-        this.times.push([avgB.slice(), avgL.slice(), avgD.slice()]);
-        this.times.push([avgB.slice(), avgL.slice(), avgD.slice()]);
-        this.times.push([avgB.slice(), avgL.slice(), avgD.slice()]);
-        this.times.push([avgB.slice(), avgL.slice(), avgD.slice()]);
-        
+        this.times.push([avgB.slice(), avgL.slice(), avgD.slice()]); //monday
+        this.times.push([avgB.slice(), avgL.slice(), avgD.slice()]);//tues
+        this.times.push([avgB.slice(), avgL.slice(), avgD.slice()]);//wed
+        this.times.push([avgB.slice(), avgL.slice(), avgD.slice()]);//thurs
+        //friday
         if(this.dayDict["Friday"]){
             this.times.push([avgB.slice(), avgL.slice(), [17, 19]]);
         }
         else{
             this.times.push([ avgB.slice(),avgL.slice(), null]);
         }
+        //saturday
         if(this.dayDict["Saturday"]){
             this.times.push([ [9,11], [11.5, 14], [16.75, 18.25]]);
         }
         else{
             this.times.push(null);
         }
+        this.times = this.exceptions(this.times, this.name);
     }
+
+    exceptions (times, name) {
+        //WHEN TO USE EXCEPTIONS
+        //at all times, for each servery, there needs to be some time that the servery opens
+        //so that whenWillOpen works
+
+        //times is an array ordered [Sun,M,Tu,W,Th,F,Sat]
+        //d.getDay() returns a number corresponding to what day of the week it is
+        //with the same indices as in times. 
+        //(that is, Sunday returns 0, which is the index of Sunday in times)
+        var d = new Date();
+        var year = d.getFullYear(); //year is 2019 or 2020 or whatever
+        var month = d.getMonth(); //month is 0-11; January = 0; December = 11
+        var date = d.getDate(); //date is 1-31
+        var day = d.getDay(); //returns number 0-6 == idx of [Sun,M,Tu,W,Th,F,Sat]
+
+        
+        //EDIT BELOW BASED ON WHEN IT IS
+        if(year == 2019 && month == 0 ){
+            //january 2019
+            if(date < 6 && day > 1){ //before Jan 6, but after Sunday
+                // I set Sunday open for Seibel/North
+                if(times[0]){
+                    times[0][1] = null; //no lunch on Jan 6
+                }
+                //leave times[1] (Monday) untouched so that for "When will open"
+                //it knows Monday is open for Baker/Sid,
+                times[2] = null;
+                times[3] = null;
+                times[4] = null;
+                times[5] = null;
+                times[6] = null; 
+            }
+            else if(date == 6){
+                if(times[0]){
+                    times[0][1] = null; //no lunch on Jan 6
+                }            
+            }
+        };
+        return times
+    }
+
     getName (){
         return this.name;
     }
@@ -72,7 +113,6 @@ class Servery {
                 return [true, "Lunch"];
             }
             else if(this.times[day][2] &&  this.times[day][2][0] && hourMin >= this.times[day][2][0] && hourMin <= this.times[day][2][1]){
-                //console.log("wassup");
                 return [true, "Dinner"];
             }
             else{
@@ -82,7 +122,6 @@ class Servery {
         else{
             return [false, null];
         }
-        // console.log(day, hourMin);
     }
 
     isOpenException (){
@@ -100,7 +139,6 @@ class Servery {
         while(searching){
             //make sure I can access this.times[dayCount].
             dayCount = dayCount % 7;
-
             //if the servery is open today
             if(this.times[dayCount]){
                 //if there's breakfast
@@ -111,7 +149,7 @@ class Servery {
                 }
                 
                 //if it's before lunch
-                else if(hourMin <= this.times[dayCount][1][0] && function(){console.log("test");}){
+                else if(this.times[dayCount][1]&&hourMin <= this.times[dayCount][1][0] ){
                     searching = false;
                     whichMeal = 1;
                     return [dayCount % 7, whichMeal, this.times[dayCount][whichMeal][0]];
